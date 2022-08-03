8c33d8b9e9b16424c80a0c6d42d1b668fe8a2526
connetium-fatih <fatih16bilgin@gmail.com>
2022-08-03 10:20:08 +0300

    first commit

---
+++ Netflix Clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Netflix Clone
+//
+//  Created by Fatih Bilgin on 3.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
