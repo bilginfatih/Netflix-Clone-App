aeae54a189bda2163b3758670700ddb1b6581043
connetium-fatih <fatih16bilgin@gmail.com>
2022-08-09 14:40:51 +0300

    UpcomingView yapildi

---
+++ Netflix Clone/ViewModels/TitleViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  TitleViewModel.swift
+//  Netflix Clone
+//
+//  Created by Fatih Bilgin on 9.08.2022.
+//
+
+import Foundation
+
+struct TitleViewModel {
+    let titleName: String
+    let posterURL: String
+}
